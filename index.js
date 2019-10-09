e54b8d1ce5ba11e12b3a4f1b3d94ec0724ff067f
nasjones <nasjonez1@gmail.com>
2019-10-09 15:35:27 -0400

    finished checkpoint

---
+++ index.js
@@ -0,0 +1,32 @@
+$(function () {
+    $("#js-shopping-list-form").submit(e => {
+      e.preventDefault();
+      const itemStyle = [];
+      const item = $(e.currentTarget).find('input[name="shopping-list-entry"]').val();
+  
+      itemStyle.push($(`<li>
+          <span class="shopping-item">${item}</span>
+          <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+              <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+              <span class="button-label">delete</span>
+            </button>
+          </div>
+        </li>`));
+  
+      $(".shopping-list").append(itemStyle);
+    });
+  
+    $('.shopping-item-toggle').on('click', e => {
+  
+      $(e.currentTarget).parent().siblings(".shopping-item").toggleClass("shopping-item__checked");
+    });
+  
+    $('.shopping-item-delete').on('click', e => {
+      $(e.currentTarget).closest("li").remove();
+  
+    });
+  
+  });
